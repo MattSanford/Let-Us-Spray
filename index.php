3046a4743b3d2d16fbe44b454036acf817a64628
Matt Sanford <pucksmasher@verizon.net>
2013-08-22 14:58:12 -0400

    test ads

--- index.php
+++ index.php
@@ -153,5 +153,16 @@
 	</div>
 </div>
 	<script src="/js/animations.js"></script>
+	<script type="text/javascript"><!--
+google_ad_client = "ca-pub-4516501803385461";
+/* test */
+google_ad_slot = "7618200230";
+google_ad_width = 120;
+google_ad_height = 240;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
 </body>
 </html>
