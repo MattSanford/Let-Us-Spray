0df4a1ae1780f37a678eda9e738ceb0cf0fc2732
Matt Sanford <pucksmasher@verizon.net>
2013-08-30 10:48:19 -0400

    collapsible

--- index.php
+++ index.php
@@ -83,22 +83,21 @@
 		<div class="ribbon-back-left-h"></div>
 		<div class="ribbon-back-right"></div>
 	</div>
-					<div id="welcome first">
-						<h1>Welcome!</h1>
-						<h2 class="bg-header">Your Non-Pressure Roof Cleaning Specialists</h2>
-						<div class="icon-side">
+					<div class="welcome" id="first">
+						<h1 class="bg-header">Your Non-Pressure Roof Cleaning Specialists</h1>
+						<div><div class="icon-side">
 							<span class="iconic house"></span>
-							<h1>X 10,000</h1
-								>
+							<span>X 10,000</span>
 						</div>
 						<div class="text-side">
 							<p>Our proven "SoftWash System" has gently cleaned more than 50,000 properties nation wide since the early 1980's.  Because we do not use a power washer, our SoftWash System will not cause any damage to your roof shingles or siding.  The SoftWash System will help to protect your valuable investment as well as increase value and curb appeal.</p>
-						</div>
+						</div></div>
+						
 							<div class="clear-fix"></div>
 					</div>
-					<div id="century21 second">
-						<h2 class="bg-header">SoftWashing - 21<sup>st</sup> Century Technology</h2>
-						<div class="text-side">
+					<div class="century21" id="second">
+						<h2 class="bg-header">SoftWashing - 21st Century Technology</h2>
+						<div><div class="text-side">
 							<p>Soft-washing is a cleaning process which utilizes ultra low pressure to deliver a customized cleaning solution, specially blended to meet the demands of each project. It differs from pressure washing in that the cleaning solution is applied and rinsed at extremely low pressure which will not harm or damage painted surfaces, vinyl siding, wood, nor contribute to granular loss of composite roof shingles.</p>
 						<div class="icon-side">
 							<h1>$$$</h1>
@@ -107,12 +106,13 @@
 						</div>						
 							<p>The power of chemistry is what makes our Soft-wash process so effective. Different surfaces and different contaminants require different levels of "soft washing". With each cleaning job we perform, our cleaning solution is custom mixed on site and applied. We do not have a "one size fits all" approach to cleaning. We will customize a cleaning plan to maximize effectiveness while also keeping your cost as low as possible!</p>
 						</div>
-
+</div>
+						
 							<div class="clear-fix"></div>		
 					</div>
 					<div class="about" id="third">
 						<h2 class="bg-header" id="about">About Us</h2>
-						<p>Established in 2004, Let Us Spray SoftWash is a Christian, family owned and operated business conveniently located in Henrico, VA.</p>
+						<div><p>Established in 2004, Let Us Spray SoftWash is a Christian, family owned and operated business conveniently located in Henrico, VA.</p>
 							<script type="text/javascript"
 src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 </script><script type="text/javascript"><!--
@@ -128,13 +128,13 @@ google_ad_height = 240;
 							<a href="http://www.bbb.org/richmond/business-reviews/roof-cleaning-and-stain-removal/let-us-spray-powerwashing-in-richmond-va-21017473"><div id="bb-wrap"></div></a>
 							<h4>Click the Image!</h4>
 						</div>	
-						<p>Providing for both residential and light commercial clients, we are fully insured, licensed, rated A+ with the Richmond BBB, and a Certified Member of SoftWash Systems.</p>
+						<p>Providing for both residential and light commercial clients, we are fully insured, licensed, rated A+ with the Richmond BBB, and a Certified Member of SoftWash Systems.</p></div>
 						<div class="clear-fix"></div>		
 					
 					</div>
 					<div class="technology" id="fourth">
 						<h2 class="bg-header" id="technology">The Unique Soft Wash System</h2>
-						<p>Dark stains on your roof are caused primarily by a persistent type of algae, Gloeocapsa Magma. The SoftWash System gently removes the staining without harming the roof itself.  AND it kills the algae and other harmful organisms so the stains don't come back!</p>
+						<div><p>Dark stains on your roof are caused primarily by a persistent type of algae, Gloeocapsa Magma. The SoftWash System gently removes the staining without harming the roof itself.  AND it kills the algae and other harmful organisms so the stains don't come back!</p>
 						<ol>
 							<li>The microorganisms that cause roof staining actually have tiny root systems which physically break down asphalt and composite roof shingles, allowing larger types of organisms to grow on your roof. The SoftWash System kills these roots and applies an algaecide and fungicide to protect against future attacks.</li>
 							<li>SoftWash uses sodium hypochlorite (bleach), which is necessary to kill harmful algae, bacteria, mold, and fungus with nearly 100% efficiency.</li>
@@ -142,26 +142,28 @@ google_ad_height = 240;
 							<li>Sodium hypochlorite is the same chemical used to treat our drinking water and sanitize and brighten our swimming pools. The notion that it will harm your roof is a scare tactic.</li>
 							<li>SoftWash application is very fast and you'll see results immediately!</li>
 							<li>Our cleaning solution is applied with an electric pump, which greatly minimizes noise and pollution.</li>
-						</ol>
+						</ol></div>
+						
 					</div>
 					<div class="FAQ" id="fifth">
 						<h2 class="bg-header">FAQ's</h2>
-						<h3 class="question">"What causes the black marks on my roof?"</h3>
+						<div><span class="question">"What causes the black marks on my roof?"</span>
 						<p>Most roof stains are caused by the growth of algae, lichen colonies or moss that feed on organic matter frequently found on roof surfaces. The portions of a roof that receive the least amount of sunlight are more likely to have roof stains first. This explains why some homes may have roof stains only on one side of the roof, typically the northern and/or western exposure or in heavily shaded areas.</p>
-						<h3 class="question">"Are you going to pressure wash my roof?"</h3>
+						<span class="question">"Are you going to pressure wash my roof?"</span>
 						<p>No, we highly recommend that you <em>NEVER</em> allow anyone to pressure wash your roof at <em>ANY</em> pressure.  We often compete against contractors who bid much lower than us.  That is, until the homeowner finds out that they are going to pressure wash their roof!  We use a soft washing system that uses no more pressure than a residential garden hose.  As a matter of fact our soft washing system is approved by ARMA (Asphalt Roofing Manufacturing Association) and is recommended by most of the largest insurance companies as a form of general maintenance.</p>
-						<h3 class="question">"How long before I will see it work?"</h3>
+						<span class="question">"How long before I will see it work?"</span>
 						<p>In most cases you will see a dramatic improvement within minutes of our Roof Renew application.  If the roof is really dirty then it may require a moderate rain to gently wash off the dead algae.  If your roof has actively growing green moss then you will see what looks like popcorn on the shingle after we have applied our Roof Renew solution.  After we kill the moss it will take a few weeks for the root system to dry up release the roof granules.  The only way to remove moss immediately is to use water pressure greater than your garden hose, and that is definitely not compatible with your roof warranty or it's longevity.</p>						
-						<h3 class="question">"How long will the washing last?"</h3>
+						<span class="question">"How long will the washing last?"</span>
 						<p>In most cases our Roof Renew solution will keep your roof clean of black streaks for approximately 4-6 years, depending on how many trees you have around your house. We offer a 5-year limited warranty when you opt to have the entire surface of your roof treated.</p>
-						<h3 class="question">"Do I need to be home when you wash my roof?"</h3>
+						<span class="question">"Do I need to be home when you wash my roof?"</span>
 						<p>No, the only thing we need is access to a water faucet and to make sure that all pets are indoors, and that all windows and doors are closed. On the other hand, if you would like to be home and watch this amazing process, that would be ok too.</p>
-						<h3 class="question">"Will your chemicals kill my plants?"</h3>
-						<p>If used improperly there can be some damage to vegetation. That is also why we like to ask all of our customers to water their plants heavily the night before and just after treatment.  In most cases you will not notice any damage to your plants.  In a very few cases there could be a light browning in a few areas that will go away quickly.</p>
+						<span class="question">"Will your chemicals kill my plants?"</span>
+						<p>If used improperly there can be some damage to vegetation. That is also why we like to ask all of our customers to water their plants heavily the night before and just after treatment.  In most cases you will not notice any damage to your plants.  In a very few cases there could be a light browning in a few areas that will go away quickly.</p></div>
+						
 					</div>
 					<div class="why" id="sixth">
 						<h2 class="bg-header">Why should I have my roof cleaned?</h2>
-							<h1>top 10 reasons to have your roof cleaned</h1>
+							<div><h1>top 10 reasons to have your roof cleaned</h1>
 							<ol>
 								<li>Improve your curb appeal!  </li>
 								<li>Decrease Utility costs!</li>
@@ -173,7 +175,7 @@ google_ad_height = 240;
 								<li>Clean it, Don't replace it!</li>
 								<li>Our team only uses Softwash Systems products.</li>
 								<li>Our team is Softwash Systems certified!</li>
-							</ol>
+							</ol></div>
 					</div>
 				</div>
 				<section class="team content">
@@ -203,5 +205,24 @@ google_ad_height = 240;
 
 	<script src="/js/analytics.js"></script>
 	<script type="text/javascript" src="/js/jquery.collapsible.min.js"></script>
+	<script type="text/javascript">
+
+			$(function() {
+				$('#first').collapsible('accordion-open', {
+				});
+				$('#second').collapsible('accordion-open', {
+				});
+				$('#third').collapsible('accordion-open', {
+				});
+				$('#fourth').collapsible('accordion-open', {
+				});
+				$('#fifth').collapsible('accordion-open', {
+				});
+				$('#sixth').collapsible('accordion-open', {
+					contentOpen: 1
+				});
+			});
+		
+	</script>
 </body>
 </html>
